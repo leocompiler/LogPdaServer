0323b73c12643c059225507ba7c1d3525c08224a
Leonardo Alexandre <leo.compiler@gmail.com>
2012-01-25 13:56:52 -0200

    Modificação inicial para branch developer .

---
+++ src/com/android/leocompiler/logpdaserver/client/LogPdaServer.java
@@ -0,0 +1,13 @@
+package com.android.leocompiler.logpdaserver.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class LogPdaServer implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		//TODO: criar a inicialização do servlet...
+
+	}
+
+}
